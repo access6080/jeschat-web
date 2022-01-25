c003e4a2dfdeed8ef651f080cbd4006d46878cee
access6080 <access.6080@gmail.com>
2022-01-24 22:34:43 -0500

    Chat Completion: Deploy

--- src/hooks/useCurrentUser.jsx
+++
@@ -1,3 +0,0 @@
-import { useSelector } from 'react-redux';
-
-export const useCurrentUser = () => useSelector(state => state.auth.user);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './auth';
-import chatReducer from './chat';
 
 export default configureStore({
   reducer: {
     auth: authReducer,
-    chat: chatReducer
   }
 })
